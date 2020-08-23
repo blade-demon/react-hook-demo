2dab80fea00b6cee4ed1defc48983b03b2853b77
blade-demon <xuziwei89@gmail.com>
2020-08-23 17:45:38 +0800

    feat: class component subscribe browser API resize event

--- src/ClassComponent/ClassComponent.js
+++ src/ClassComponent/ClassComponent.js
@@ -10,18 +10,25 @@ export default class Greeting extends React.Component {
       name: "Mary",
       surname: "Petter",
       locale: "en",
+      width: window.innerWidth,
     };
 
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleSurNameChange = this.handleSurNameChange.bind(this);
+    this.handleResize = this.handleResize.bind(this);
   }
 
   componentDidMount() {
     document.title = this.state.name + " " + this.state.surname;
+    window.addEventListener("resize", this.handleResize);
   }
 
   componentDidUpdate() {
     document.title = this.state.name + " " + this.state.surname;
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize");
   }
 
   handleNameChange(e) {
@@ -33,6 +40,12 @@ export default class Greeting extends React.Component {
   handleSurNameChange(e) {
     this.setState({
       surname: e.target.value,
+    });
+  }
+
+  handleResize() {
+    this.setState({
+      width: window.innerWidth,
     });
   }
 
@@ -53,10 +66,13 @@ export default class Greeting extends React.Component {
             <LocaleContext.Consumer>
               {(locale) => (
                 <Row label="locale">
-                  <input value={locale} disabled/>
+                  <input value={locale} disabled />
                 </Row>
               )}
             </LocaleContext.Consumer>
+            <Row label="Width">
+              <input value={this.state.width} disabled />
+            </Row>
           </section>
         )}
       </ThemeContext.Consumer>
