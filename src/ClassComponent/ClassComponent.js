c30381cf5aa4f5004dc9bb96e6cd34505a8dbe35
blade-demon <xuziwei89@gmail.com>
2020-09-07 12:48:01 +0800

    step1:hook走起来

--- src/ClassComponent/ClassComponent.js
+++ src/ClassComponent/ClassComponent.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Row from "../Row/Row";
-import { ThemeContext, LocaleContext } from "../Context";
 
 export default class Greeting extends React.Component {
   constructor(props) {
@@ -8,27 +7,9 @@ export default class Greeting extends React.Component {
 
     this.state = {
       name: "Mary",
-      surname: "Petter",
-      locale: "en",
-      width: window.innerWidth,
     };
 
     this.handleNameChange = this.handleNameChange.bind(this);
-    this.handleSurNameChange = this.handleSurNameChange.bind(this);
-    this.handleResize = this.handleResize.bind(this);
-  }
-
-  componentDidMount() {
-    document.title = this.state.name + " " + this.state.surname;
-    window.addEventListener("resize", this.handleResize);
-  }
-
-  componentDidUpdate() {
-    document.title = this.state.name + " " + this.state.surname;
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.handleResize);
   }
 
   handleNameChange(e) {
@@ -37,45 +18,13 @@ export default class Greeting extends React.Component {
     });
   }
 
-  handleSurNameChange(e) {
-    this.setState({
-      surname: e.target.value,
-    });
-  }
-
-  handleResize() {
-    this.setState({
-      width: window.innerWidth,
-    });
-  }
-
   render() {
     return (
-      <ThemeContext.Consumer>
-        {(theme) => (
-          <section className={theme}>
-            <Row label="Name">
-              <input value={this.state.name} onChange={this.handleNameChange} />
-            </Row>
-            <Row label="Surname">
-              <input
-                value={this.state.surname}
-                onChange={this.handleSurNameChange}
-              />
-            </Row>
-            <LocaleContext.Consumer>
-              {(locale) => (
-                <Row label="locale">
-                  <input value={locale} disabled />
-                </Row>
-              )}
-            </LocaleContext.Consumer>
-            <Row label="Width">
-              <input value={this.state.width} disabled />
-            </Row>
-          </section>
-        )}
-      </ThemeContext.Consumer>
+      <section>
+        <Row label="Name">
+          <input value={this.state.name} onChange={this.handleNameChange} />
+        </Row>
+      </section>
     );
   }
 }
--- src/FunctionalComponent/FunctionalComponent.js
+++ src/FunctionalComponent/FunctionalComponent.js
@@ -1,61 +1,14 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState } from "react";
 import Row from "../Row/Row";
-import { ThemeContext, LocaleContext } from "../Context";
 
 export default function Greeting(props) {
-  const name = useFormInput("Mary");
-  const surname = useFormInput("Petter");
-  const theme = useContext(ThemeContext);
-  const locale = useContext(LocaleContext);
-  const width = useWindowWidth();
-  useDocumentTitle(name.value + " " + surname.value);
-
+  const [name, setName] = useState("Mary");
 
   return (
-    <section className={theme}>
+    <section>
       <Row label="Name">
-        <input {...name} />
-      </Row>
-      <Row label="Surname">
-        <input {...surname} />
-      </Row>
-      <Row label="Language">
-        <input value={locale} disabled />
-      </Row>
-      <Row label="Width">
-        <input value={width} disabled />
+        <input value={name} onChange={(e) => setName(e.target.value)} />
       </Row>
     </section>
   );
-}
-
-function useFormInput(initialValue) {
-  const [value, setValue] = useState(initialValue);
-
-  function handleChange(e) {
-    setValue(e.target.value);
-  }
-
-  return {
-    value,
-    onChange: handleChange
-  }
-}
-
-function useDocumentTitle(title) {
-  useEffect(() => {
-    document.title = title;
-  });
-}
-
-function useWindowWidth() {
-  const [width, setWidth] = useState(window.innerWidth);
-  useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth);
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  });
-  return width;
 }
