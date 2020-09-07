21549bd6bff520670ded428a05acba1981ff6fd1
blade-demon <xuziwei89@gmail.com>
2020-09-07 20:41:23 +0800

    step6: complete all

--- src/ClassComponent/ClassComponent.js
+++ src/ClassComponent/ClassComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Row from "../Row/Row";
+import { ThemeContext, LocaleContext } from "../Context";
 
 export default class Greeting extends React.Component {
   constructor(props) {
@@ -7,9 +8,33 @@ export default class Greeting extends React.Component {
 
     this.state = {
       name: "Mary",
+      surName: "Petter",
+      width: window.innerWidth,
     };
 
     this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleSurNameChange = this.handleSurNameChange.bind(this);
+    this.handleWith = this.handleWith.bind(this);
+  }
+
+  componentDidMount() {
+    document.title = this.state.name + " " + this.state.surName;
+
+    window.addEventListener("resize", this.handleWith);
+  }
+
+  handleWith() {
+    this.setState({
+      width: window.innerWidth,
+    });
+  }
+
+  componentDidUpdate() {
+    document.title = this.state.name + " " + this.state.surName;
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleWith);
   }
 
   handleNameChange(e) {
@@ -18,13 +43,46 @@ export default class Greeting extends React.Component {
     });
   }
 
+  handleSurNameChange(e) {
+    this.setState({
+      surName: e.target.value,
+    });
+  }
+
   render() {
     return (
-      <section>
-        <Row label="Name">
-          <input value={this.state.name} onChange={this.handleNameChange} />
-        </Row>
-      </section>
+      <ThemeContext.Consumer>
+        {(theme) => {
+          return (
+            <section className={theme}>
+              <Row label="Name">
+                <input
+                  value={this.state.name}
+                  onChange={this.handleNameChange}
+                />
+              </Row>
+              <Row label="surName">
+                <input
+                  value={this.state.surName}
+                  onChange={this.handleSurNameChange}
+                />
+              </Row>
+              <Row label="width">
+                <input value={this.state.width} disabled />
+              </Row>
+              <LocaleContext.Consumer>
+                {(locale) => {
+                  return (
+                    <Row label="width">
+                      <input value={locale} disabled />
+                    </Row>
+                  );
+                }}
+              </LocaleContext.Consumer>
+            </section>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
--- src/FunctionalComponent/FunctionalComponent.js
+++ src/FunctionalComponent/FunctionalComponent.js
@@ -1,13 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Row from "../Row/Row";
+import { ThemeContext, LocaleContext } from "../Context";
+
+function useDocumentTitle(initialValue) {
+  useEffect(() => {
+    document.title = initialValue;
+  });
+}
+
+function useWindowWidth(initialValue) {
+  const [width, setWidth] = useState(initialValue);
+  const handleResize = () => setWidth(window.innerWidth);
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
+  return width;
+}
+
+function useFormInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+  return {
+    value,
+    onChange: handleChange
+  };
+}
 
 export default function Greeting(props) {
-  const [name, setName] = useState("Mary");
+  const name = useFormInput("Mary");
+  const surName = useFormInput("Petter");
+  const width = useWindowWidth(window.innerWidth);
+  const theme = useContext(ThemeContext);
+  const locale = useContext(LocaleContext);
+
+  useDocumentTitle(name.value + " " + surName.value);
 
   return (
-    <section>
+    <section className={theme}>
       <Row label="Name">
-        <input value={name} onChange={(e) => setName(e.target.value)} />
+        <input {...name} />
+      </Row>
+      <Row label="surName">
+        <input {...surName} />
+      </Row>
+      <Row label="width">
+        <input value={width} disabled />
+      </Row>
+      <Row label="width">
+        <input value={locale} disabled />
       </Row>
     </section>
   );
